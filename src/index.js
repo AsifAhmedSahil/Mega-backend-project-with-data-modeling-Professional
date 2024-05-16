588bd1b18918b9c34c70f7565beb0dfeede5372e
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-05-17 02:09:24 +0600

    middlewares setup

--- src/index.js
+++ src/index.js
@@ -2,15 +2,22 @@ import dotenv from 'dotenv';
 import path from 'path';
 
 import connectDB from "./db/index.js"
+import app from './app.js';
 
-// dotenv.config({
-//     path: "./env"
-// })
+
 dotenv.config({ path: path.join((process.cwd(), '.env')) });
-// dotenv.config()
+
 // setup database professionally
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT | 5000, () =>{
+        console.log(`server is running on port: ${process.env.PORT}`)
+    });
+})
+.catch((err)=>{
+    console.log("mongodb connection failed from src/index.js",err);
+})
 
 
 
