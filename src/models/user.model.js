0db5c2b00667a56b165a972cc6441d64123f9a32
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-05-17 12:27:28 +0600

    jwt token setup and generate

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -66,4 +66,30 @@ userSchema.methods.isPasswordCorrect = async function(password) {
     return await bcrypt.compare(password,this.password)
 }
 
+// generate access token and refresh token
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign({
+        _id: this._id,
+        email:this.email,
+        username:this.username,
+        fullName:this.fullName
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+)
+}
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign({
+        _id: this._id,
+        
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    }
+)
+}
+
 export const User = mongoose.model("User",userSchema)
