8c9f3f66f41c53da368ea6bb6ebc9a614eed8fa5
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-05-17 10:54:48 +0600

    middleware and api error handler

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+const asyncHandler = (requestHandler) =>{
+    (req,res,next) =>{
+        Promise.resolve(requestHandler(req,res,next)).catch((error) => next(error))
+    }
+
+}
+
+
+export {asyncHandler}
+
+// easy method but not production friendly
+
+// const asyncHandler = (fn) => async(req,res,next) =>{
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(error.code || 400).json({
+//             success:false,
+//             message: error.message
+//         })
+//     }
+// }
