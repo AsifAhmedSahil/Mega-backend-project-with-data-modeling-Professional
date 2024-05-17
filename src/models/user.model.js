4d136d55a0f7eead0ee89953e45aa9164badbb51
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-05-17 14:45:14 +0600

    file upload function created with cloudinary

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,95 +1,97 @@
-import mongoose,{Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
 
-const userSchema = new Schema({
-    userName:{
-        type: String,
-        required: true,
-        lowercase:true,
-        unique:true,
-        trim:true,
-        index:true
-        // here index is used for database searching
+const userSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true,
+      trim: true,
+      index: true,
+      // here index is used for database searching
     },
-    email:{
-        type: String,
-        required: true,
-        lowercase:true,
-        unique:true,
-        trim:true,
-
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true,
+      trim: true,
     },
-    fullName:{
-        type: String,
-        required: true,
-        trim:true,
-        index:true
-        // here index is used for database searching
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+      // here index is used for database searching
     },
-    avatar:{
-        type:String,
-        required:true
+    avatar: {
+      type: String,
+      required: true,
     },
-    coverImg:{
-        type:String,
+    coverImg: {
+      type: String,
     },
-    watchHistory:[
-        {
-            type:Schema.Types.ObjectId,
-            ref: "Video"
-        }
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
     ],
-    password:{
-        type:String,
-        required: [true,"Password is required"]
+    password: {
+      type: String,
+      required: [true, "Password is required"],
     },
-    refreshToken:{
-        type:String
-    }
-},
-{
-    timestamps:true
-})
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 // for encrypt password
-userSchema.pre("save",async function (next) {
-    if(!this.isModified("password")) return next()
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
 
-    this.password = bcrypt.hash(this.password,10)
-    next()
-})
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
 
 // check password is correct or not
 
-userSchema.methods.isPasswordCorrect = async function(password) {
-    return await bcrypt.compare(password,this.password)
-}
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
 // generate access token and refresh token
-userSchema.methods.generateAccessToken = function(){
-    return jwt.sign({
-        _id: this._id,
-        email:this.email,
-        username:this.username,
-        fullName:this.fullName
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
     },
     process.env.ACCESS_TOKEN_SECRET,
     {
-        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
     }
-)
-}
-userSchema.methods.generateRefreshToken = function(){
-    return jwt.sign({
-        _id: this._id,
-        
+  );
+};
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
     },
     process.env.REFRESH_TOKEN_SECRET,
     {
-        expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
     }
-)
-}
+  );
+};
 
-export const User = mongoose.model("User",userSchema)
+export const User = mongoose.model("User", userSchema);
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// write a method for organizing code and upload file
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    // file uploader successfully
+
+    console.log("File uploaded successfully:", response.url);
+
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // this method use for remove locally saved file
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
