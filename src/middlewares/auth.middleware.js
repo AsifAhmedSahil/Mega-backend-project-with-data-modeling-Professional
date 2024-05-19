a752ed54ae677c8c4a56415e3a1dd13c890c991f
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-05-20 00:07:30 +0600

    middleware for loggedout user

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,5 +1,27 @@
+import { ApiError } from "../utils/ApiError";
 import { asyncHandler } from "../utils/asyncHandler";
+import jwt from "jsonwebtoken"
+import { User } from "../models/user.model";
 
 export const verifyJWT = asyncHandler(async(req,res,next) =>{
-    const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
+    try {
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
+        if(!token){
+            throw new ApiError(401,"unauthorized token")
+        }
+    
+        const decodedToken = jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
+    
+        const user = await User.findById(decodedToken._id).select("-password -refreshToken")
+    
+        if(!user){
+            throw new ApiError(401,"Invalid access token!")
+        }
+    
+        req.user = user;
+        next()
+    } catch (error) {
+        throw new ApiError(401, error?.message || "invalid access token!")
+        
+    }
 })
