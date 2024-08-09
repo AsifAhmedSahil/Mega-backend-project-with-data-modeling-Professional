70830cdc8c4a326738757a65f96a07af3523fd1c
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-08-09 11:53:32 +0600

    change password controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -217,6 +217,22 @@ const refreshAccessToken = asyncHandler(async(req,res) =>{
     }
 })
 
+const changeCurrentPassword = asyncHandler(async(req,res) =>{
+    const {oldPassword, newPassword} = req.body
+    const user = await User.findById(req.user?.id)
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
+    if(!isPasswordCorrect){
+        throw new ApiError(400,"Invalid old password!")
+    }
+
+    user.password = newPassword
+    await user.save({validateBeforeSave:false})
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200,{},"Password updated succesfully"))
+})
+
 
 
 export { 
