46257273b08f3ff9b294a5a29ba0135ffe6e4dfe
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-08-09 17:04:17 +0600

    all bug fixed & route tested

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -155,8 +155,8 @@ const loginUser = asyncHandler(async (req,res) =>{
 
 const loggedOutUser = asyncHandler(async(req,res) =>{
     await User.findByIdAndUpdate(req.user._id, {
-        $set: {
-            refreshToken: undefined
+        $unset: {
+            refreshToken: 1
         }
     },
     {
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -33,7 +33,7 @@ router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT, loggedOutUser);
 router.route("/refresh-token").post(refreshAccessToken);
 router.route("/change-password").post(verifyJWT, changeCurrentPassword);
-router.route("/current-user").post(verifyJWT, getCurrentUser);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
 router.route("/update-account").patch(verifyJWT, updateAccountDetails);
 
 router
